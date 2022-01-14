c2f5757e5692a421eb8db86fc306b92a1f7fef21
zhengxixuan <930042468@qq.com>
2022-01-14 15:28:15 +0800

    梳理

---
+++ prettier.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    printWidth: 200,
+    tabWidth: 4,
+    useTabs: false,
+    semi: false,
+    vueIndentScriptAndStyle: true,
+    singleQuote: true,
+    quoteProps: 'as-needed',
+    bracketSpacing: true,
+    trailingComma: 'es5',
+    arrowParens: 'always',
+    insertPragma: false,
+    requirePragma: false,
+    proseWrap: 'never',
+    htmlWhitespaceSensitivity: 'strict',
+    endOfLine: 'auto',
+    rangeStart: 0,
+}
